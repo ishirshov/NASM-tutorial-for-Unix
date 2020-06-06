130c02759f05be9e39423922bc049d5104a67179
Ildar Shirshov <ildar-shirshov@ya.ru>
2020-06-06 16:07:15 +0300

    Добавил пример обработки аргументов CMD, использовая регистра указателя ESP.

--- asmtutor.com/src/helloworld-inc.asm
+++ asmtutor.com/src/helloworld-inc.asm
@@ -1,8 +1,11 @@
 %include 'functions.asm'
 
+; Так как строки в памяти расположены линейно, т.е. друг
+; за дружкой sprint выводит строки пока не обнаружит символ
+; конца строки.
 SECTION .data
-msg1     db      'Hello, low level world!', 0Ah
-msg2     db      'This is now we recycle in NASM', 0Ah
+msg1     db      'Hello, low level world!', 0Ah, 0h         ; Нультерминальный символ
+msg2     db      'This is now we recycle in NASM', 0Ah, 0h  ; Нультерминальный символ
 
 SECTION .text
 global _start
