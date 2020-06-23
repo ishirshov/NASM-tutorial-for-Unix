f051d32537ed7305e8e2cb519f800fbd91626b82
Ildar Shirshov <ildar-shirshov@ya.ru>
2020-06-23 13:33:57 +0300

    Убрал опцию serial only. Добавил порт GDB для удаленной отладки.

--- start-qemu.sh
+++ start-qemu.sh
@@ -2,18 +2,17 @@
 
 SRC_DIR="$(pwd)/src"
 BOOT_DIR="$(pwd)/boot"
+GDB_PORT=5523
 
-if [ "${1}" = "serial-only" ]; then
-    EXTRA_ARGS='-nographic'
-else
-    EXTRA_ARGS='-serial stdio'
-fi
+echo "GDB сервер доступен на порту ${GDB_PORT} ..."
 
 qemu-system-i386 \
 	-M pc \
 	-kernel ${BOOT_DIR}/bzImage \
+    -net user,hostfwd=tcp::${GDB_PORT}-:123 \
 	-virtfs local,path=${SRC_DIR},mount_tag=host0,security_model=none,id=host0 \
 	-drive file=${BOOT_DIR}/rootfs.ext2,if=virtio,format=raw \
 	-append "rootwait root=/dev/vda console=tty1 console=ttyS0" \
 	-net nic,model=virtio \
-	-net user  ${EXTRA_ARGS}
+	-net user \
+    -nographic
