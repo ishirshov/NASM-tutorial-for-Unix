0c200e194418705a2cebfaee759bf30a50ac6c17
Ildar Shirshov <ildar-shirshov@ya.ru>
2020-06-23 23:09:18 +0300

    Добавил монтирование vfat каталога с исходным кодом проекта.

--- start-qemu.sh
+++ start-qemu.sh
@@ -4,13 +4,22 @@ SRC_DIR="$(pwd)/src"
 BOOT_DIR="$(pwd)/boot"
 GDB_PORT=5523
 
+if [[ "${OSTYPE}" == "darwin"*  ]]; then
+    VIRTFS_OPTION="-drive format=raw,file=fat:rw:${SRC_DIR}"
+elif [[ "${OSTYPE}" == "linux-gnu"* ]]; then
+    VIRTFS_OPTION="-virtfs local,path=${SRC_DIR},mount_tag=host0,security_model=none,id=host0"
+else
+    echo "Ошибка! Платформа ${OSTYPE} не поддерживается ..." >&2
+    exit -1
+fi
+
 echo "GDB сервер доступен на порту ${GDB_PORT} ..."
 
 qemu-system-i386 \
 	-M pc \
 	-kernel ${BOOT_DIR}/bzImage \
     -net user,hostfwd=tcp::${GDB_PORT}-:123 \
-	-virtfs local,path=${SRC_DIR},mount_tag=host0,security_model=none,id=host0 \
+    ${VIRTFS_OPTION} \
 	-drive file=${BOOT_DIR}/rootfs.ext2,if=virtio,format=raw \
 	-append "rootwait root=/dev/vda console=tty1 console=ttyS0" \
 	-net nic,model=virtio \
